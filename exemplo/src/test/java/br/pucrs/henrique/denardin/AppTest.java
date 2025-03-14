8bcc7a467376d8410d2a602c38e5622c6acdd355
HenriqueDenardin <136604147+HenriqueDenardin@users.noreply.github.com>
2025-03-14 00:52:56 +0000

    teste1

---
+++ exemplo/src/test/java/br/pucrs/henrique/denardin/AppTest.java
@@ -0,0 +1,22 @@
+package br.pucrs.henrique.denardin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
